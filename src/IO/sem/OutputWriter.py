03cb28cfb0dd29d7ba267c10405d4eee55e06111
gabinoumbe <oumbegabin@yahoo.fr>
2025-09-10 10:29:33 +0200

    add failure when output mismatches the expected mimetype

--- src/IO/sem/OutputWriter.py
+++ src/IO/sem/OutputWriter.py
@@ -2,18 +2,35 @@ import os
 import json
 import logging
 import zipfile
+import mimetypes
 
 from src.IO.MappingAbortionError import MappingAbortionError
+from src.util import is_zipfile
 
 
 class OutputWriter:
+
+    @staticmethod
+    def remove_file(file_path):
+        try:
+            os.remove(file_path)
+            logging.info(f"{file_path} has been deleted.")
+        except (FileNotFoundError, PermissionError, IsADirectoryError, OSError) as e:
+            logging.warning(f"{file_path} was not deleted: {e}")
+            raise MappingAbortionError(f"Failed to delete file '{file_path}'.")
 
     @staticmethod
     def save_the_file(mapped_metadata, file_path):
         try:
             with open(file_path, 'w', encoding="utf-8") as json_file:
                 json.dump(mapped_metadata, json_file, indent=4, ensure_ascii=False)
-            logging.info("The output document has been created successfully!")
+            mt, _ = mimetypes.guess_type(file_path)
+            if not mt == "application/json":
+                OutputWriter.remove_file(file_path)
+                logging.error(f"Wrong output mimetype. Expecting an 'application/json'! Please check if the output path '{file_path}' you provide has the right extension.")
+                raise MappingAbortionError(f"'{file_path}' has a wrong mimetype. Produced output is corrupted! Please check the output path extension.")
+            else:
+                logging.info("The output document has been created successfully!")
         except (FileNotFoundError, PermissionError, IsADirectoryError, OSError, TypeError, ValueError) as e:
             logging.error(f"Unable to save {file_path}: {e}")
             raise MappingAbortionError(f"Failed to save {file_path}.")
@@ -30,16 +47,18 @@ class OutputWriter:
                     except (FileNotFoundError, PermissionError, IsADirectoryError, OSError, zipfile.BadZipFile) as e:
                         logging.error(f"Adding {file_path} to zip was not successful: {e}")
                         raise MappingAbortionError(f"Failed to add {file_path} to zip.")
-            logging.info(f"Files have been zipped into {zip_file_path} sucessfully!")
-        except MappingAbortionError as e:
-            logging.error(f"Failed to create zip file at {zip_file_path}: {e}")
-            raise MappingAbortionError(f"Failed to save to zip.")
+            mt, _ = mimetypes.guess_type(zip_file_path)
+            if not mt == "application/zip":
+                OutputWriter.remove_file(zip_file_path)
+                logging.error(f"Wrong output mimetype. Expecting an 'application/zip'! Please check if the output path '{zip_file_path}' you provide has the right extension.")
+                raise MappingAbortionError(f"'{zip_file_path}' has a wrong mimetype. Produced output is corrupted! Please check the output path extension.")
+            else:
+                logging.info(f"Files have been zipped into '{zip_file_path}' sucessfully!")
             
-        # Delete the original files after zipping
-        for file_path in file_path_list:
-            try:
-                os.remove(file_path)
-                logging.info(f"{file_path} has been deleted.")
-            except (FileNotFoundError, PermissionError, IsADirectoryError, OSError) as e:
-                logging.warning(f"{file_path} to zip was not deleted: {e}")
-                raise MappingAbortionError(f"Failed to delete file {file_path} after zip.")
+        except MappingAbortionError as e:
+            logging.error(f"Failed to create a correct zip file: {e}")
+            raise MappingAbortionError(f"Failed to save to zip.")
+        finally:
+            # Delete the original files after zipping
+            for file_path in file_path_list:
+                OutputWriter.remove_file(file_path)
